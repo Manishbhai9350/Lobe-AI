83e7b8cc7ce1a37362f374618f4d0b94fcedcb6f
Manish <keshraram294@gmail.com>
2025-08-05 21:03:11 +0530

    05 AI Jobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          
+          <Toaster />
           {children}
         </body>
       </html>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,27 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+  { id: "Gather-Text" },
+  { event: "gather/text" },
   async ({ event, step }) => {
-    await step.sleep("Gathering Data", "20s");
-    await step.sleep("Filtering Data", "10s");
-    await step.sleep("Pushing Data", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    
+    
+    const Gatherer =  createAgent({
+      name:'gatherer',
+      system:"You are an expert summarizer. and you summarize into two words.",
+      model: openai({model:"gpt-4o"})
+    })
+
+    console.log(event.data.text)
+    console.log(process.env.OPENAI_API_KEY)
+
+    const {output} = await Gatherer.run(`
+        Summarize the following text: ${event.data.text}
+      `)
+
+    console.log(output)
+
+    return { output };
   },
 );
